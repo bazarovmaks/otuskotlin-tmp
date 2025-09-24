a8991d1d757c15ab48355475734b2e3f08412417
m.bazarov <m.bazarov@qiwi.com>
2025-09-24 22:40:19 +0300

    m1l1 First App

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") apply false
 }
 
 group = "ru.bazarovmaks.otuskotlin.marketplace"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(24)
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
 }
---
+++ m1l1-first/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FirstTest {
+    @Test
+    fun test() {
+        assertEquals(5, 3 + 2)
+    }
+}
